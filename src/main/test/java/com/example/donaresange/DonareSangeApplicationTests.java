d9b7bfb428ab62cce0771484b23d345eec5d875d
alexiagiesswein <95859387+alexiagiesswein@users.noreply.github.com>
2023-05-05 02:07:25 +0300

    second commit

---
+++ src/main/test/java/com/example/donaresange/DonareSangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.donaresange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonareSangeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
